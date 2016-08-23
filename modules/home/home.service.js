1c2b9cdd065079b86fd72263548f1573d14a4055
Mariem <mariohel@hotmail.com>
2016-08-23 14:17:12 +0200

    removing reset function as it is not needed anymore

--- modules/home/home.service.js
+++ modules/home/home.service.js
@@ -30,37 +30,7 @@
 			return deferred.promise;
 	
 		};
-		
-		factory.resetDataList = function () {
-			
-			var deferred = $q.defer();
-			
-			if (factory._data)
-				deferred.resolve(factory._data);
-			else{
-				$http.get('db/data.json')
-				.then(function (data) {
-					factory._data=data;
-					$http.post('http://localhost:3000/crossover')
-					.then(function (data) {
-						deferred.resolve(data);
-					})
-					.catch(function (error) {
-						deferred.reject(error);
-					});
-					
-				})
-				.catch(function (error) {
-					deferred.reject(error);
-				});
-	
-			}
-	
-			
-			return deferred.promise;
-	
-		};
-		
+				
 		factory.addItem = function (item) {
 			
 			var deferred = $q.defer();
